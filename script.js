575404dc08ec2356fcfb34eecfaa6895f0bbe2d5
mkokkatt <47791277+mkokkatt@users.noreply.github.com>
2020-10-27 13:08:41 -0400

    Text Box to Color Swatch Functionality Added

--- script.js
+++ script.js
@@ -1,17 +1,103 @@
 
 
-		/*
-jq(".colorswatch").click(function() {
+		
+
+
+var jq = jQuery.noConflict();
+	jq(document).ready(function(){
+/*
+		jq(".colorswatch").click(function() {
 
            var x = Math.floor(Math.random() * 256); // range is 0-255
     		var y = Math.floor(Math.random() * 256);
     		var z = Math.floor(Math.random() * 256);
     		var thergb = "rgb(" + x + "," + y + "," + z + ")"; 
 
-			jq(this).css("background-color",thergb);		});
+			jq(this).css("background-color",thergb);		});*/
+
+				function setColor() {
+
+           var x = Math.floor(Math.random() * 256); // range is 0-255
+    		var y = Math.floor(Math.random() * 256);
+    		var z = Math.floor(Math.random() * 256);
+    		var thergb = "rgb(" + x + "," + y + "," + z + ")"; 
+
+    		var colors = new Array(x, y, z);
+			jq(".colorswatch").css("background-color",thergb);	
+			return colors
+				}
+
+				setColor();
+/*
+
+
+
+	 function printColor() {
+	 	var arr = setColor();
+	 
+	   jq(".colorswatch").after("red: " + arr[0] + " green: " + arr[1] + " blue: " + arr[2]);
+
+
+	 }
+
+			setColor();
+			printColor();*/
+/*
+var colorObj = {
+	red: 0,
+	green: 0,
+	blue: 0
+};
+
+jq('#red').on('input', function() {
+	var input=jq(this);
+	var color=input.val();
+	colorObj.red = color;
+
+});
+jq('#green').on('input', function() {
+	var input=jq(this);
+	var color=input.val();
+	colorObj.green = color;
+
+});
+jq('#blue').on('input', function() {
+	var input=jq(this);
+	var color=input.val();
+	colorObj.blue = color;
+
+});
+
+
+
+
+
+
+
+
+var rgb = "rgb(" + values[0] + "," + values[1] + "," + values[2] + ")"; 
+
+jq(".colorswatch").css("background-color",rgb);	
+
 */
-var jq = jQuery.noConflict();
-	jq(document).ready(function(){
+
+document.getElementById('colors').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  // your code here
+  var x = document.getElementById('red').value;
+  var y = document.getElementById('green').value;
+  var z = document.getElementById('blue').value;
+
+  // Calculation
+  var rgb = "rgb(" + x + "," + y + "," + z + ")"; 
+  // Show Result
+  jq(".colorswatch").css("background-color",rgb);	
+  document.getElementById("r1").innerHTML = rgb;
+
+  return false;
+});
+
  /*
     function hexFromRGB(r, g, b) {
       var hex = [
@@ -26,6 +112,8 @@ var jq = jQuery.noConflict();
       });
       return hex.join( "" ).toUpperCase();
     }*/
+
+    /*
 function pickColor() {
 
 
@@ -34,7 +122,7 @@ function pickColor() {
     		var z = Math.floor(Math.random() * 256);
     		var colors = new Array(x, y, z);
     		return colors;
-}
+}*/
 /*
 function refreshSwatch() {
       var red = $( "#red" ).slider( "value" ),
@@ -59,7 +147,7 @@ $( "#red, #green, #blue" ).slider({
     $( "#blue" ).slider( "value", x[2]);
 */
 
-
+/*
 $( function() {
     $( "#red" ).slider({
       orientation: "vertical",
@@ -72,7 +160,7 @@ $( function() {
       }
     });
     $( "#amount" ).val( $( "#red" ).slider( "value" ) );
-  } );
+  } );*/
 });
 
 
