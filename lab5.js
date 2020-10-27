196d0518f5a446d58635757944041362c714f1ef
shaneriggs <61204126+shaneriggs@users.noreply.github.com>
2020-10-27 13:31:44 -0400

    Add files via upload
    Creates colored sliders that change the color of a swatch, need to figure out how to get the values of the sliders and submit them via a form as the player's guess

--- lab5.js
+++ lab5.js
@@ -1,13 +1,49 @@
 $("document").ready(function(){
-	$("form[name='registration']").validate({
-		rules: {
-			name:"required"
-		},
-		messages: {
-			name: "Please enter your name"
-		},
-		submithandler: function(form) {
-			form.submit();
-		}
+	var swatchRed, swatchBlue, swatchGreen;
+	swatchRed = "FF";
+	swatchBlue = "00";
+	swatchGreen = "00";
+	var swatchColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+	document.getElementById("swatch").style.backgroundColor = swatchColor;
+
+	function hexFromRGB(r, g, b) {
+      var hex = [
+        r.toString( 16 ),
+        g.toString( 16 ),
+        b.toString( 16 )
+      ];
+      $.each( hex, function( nr, val ) {
+        if ( val.length === 1 ) {
+          hex[ nr ] = "0" + val;
+        }
+      });
+      return hex.join( "" ).toUpperCase();
+    }
+
+	function refreshSwatch() {
+		var red = $( "#red" ).slider( "value" ),
+			green = $( "#green" ).slider( "value" ),
+			blue = $( "#blue" ).slider( "value" ),
+			hex = hexFromRGB( red, green, blue );
+		$( "#swatch" ).css( "background-color", "#" + hex );
+	}
+
+	function updateForm() {
+		var red = $( "#red" ).slider( "value" ),
+			green = $( "#green" ).slider( "value" ),
+			blue = $( "#blue" ).slider( "value" );
+		$("#redGuess").val(red);
+	}
+
+	$( "#red, #green, #blue" ).slider({
+		orientation: "horizontal",
+		range: "min",
+		max: 255,
+		value: 127,
+		slide: refreshSwatch,
+		change: refreshSwatch
 	});
+	$( "#red" ).slider( "value", 255 );
+	$( "#green" ).slider( "value", 140 );
+	$( "#blue" ).slider( "value", 60 );
 });
