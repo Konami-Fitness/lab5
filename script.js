280ebe1ad36ff4fe918e1a9acdf288bf07f892c4
teddy-clark <edwardeclarkiii@gmail.com>
2020-10-27 22:02:25 -0400

    js with settings page

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var jq = jQuery.noConflict();
 jq(document).ready(function(){
-    
 
   var colorObj = {
     red: 0,
@@ -11,16 +10,22 @@ jq(document).ready(function(){
   var game = {
     time:"00:00:00",
     round:0
-
-//include timer and round tracker
-
   };
 
+	var player = {
+		name: "guest",
+		score: 0
+	};
 
-        
-    function get_elapsed_time_string(total_seconds) {
-  function pretty_time_string(num) {
-    return ( num < 10 ? "0" : "" ) + num;
+	document.getElementById("settingsform").addEventListener("submit", function(e) {
+		e.preventDefault();
+		player.name = document.getElementById("fname").value;
+		location.replace("play.html");
+	});
+
+  function get_elapsed_time_string(total_seconds) {
+  	function pretty_time_string(num) {
+    	return ( num < 10 ? "0" : "" ) + num;
   }
 
   var hours = Math.floor(total_seconds / 3600);
@@ -42,11 +47,11 @@ jq(document).ready(function(){
   return currentTimeString;
 }
 
-var elapsed_seconds = 0;
-setInterval(function() {
-  elapsed_seconds = elapsed_seconds + 1;
-  $('#box_header').text(get_elapsed_time_string(elapsed_seconds));
-}, 1000);
+	var elapsed_seconds = 0;
+	setInterval(function() {
+	  elapsed_seconds = elapsed_seconds + 1;
+	  $('#box_header').text(get_elapsed_time_string(elapsed_seconds));
+	}, 1000);
 
   function setColor() {
     var x = Math.floor(Math.random() * 256); // range is 0-255
@@ -72,7 +77,7 @@ setInterval(function() {
     value: 0
   });
 
-  
+
   jq('#red').on('input', function() {
     let input=jq(this);
     jq('#red-slide').slider('value', input.val())
@@ -86,28 +91,25 @@ setInterval(function() {
     jq('#blue-slide').slider('value', input.val());
   });
   jq('#red-slide').slider({
-    change: function(event, ui) { 
+    change: function(event, ui) {
        jq("#red").val(ui.value)
-    } 
+    }
   })
   jq('#green-slide').slider({
-    change: function(event, ui) { 
+    change: function(event, ui) {
        jq("#green").val(ui.value)
-    } 
+    }
   })
   jq('#blue-slide').slider({
-    change: function(event, ui) { 
+    change: function(event, ui) {
        jq("#blue").val(ui.value)
-    } 
+    }
   })
-    
-
-function percentOff(guess, correct) {
-
-  return Math.round((((Math.abs(correct - guess) / 255) * 100) + Number.EPSILON) * 100) / 100;
-}
 
 
+	function percentOff(guess, correct) {
+	  return Math.round((((Math.abs(correct - guess) / 255) * 100) + Number.EPSILON) * 100) / 100;
+	}
 
   document.getElementById('colors').addEventListener('submit', function(e) {
     e.preventDefault();
@@ -134,7 +136,7 @@ function percentOff(guess, correct) {
 
       var percentOffGreen = percentOff(y, colorObj.green);
       var percentOffBlue = percentOff(z, colorObj.blue);
-      jq('.stats').html("Red: " + percentOffRed + "% off" + "<br>" + 
+      jq('.stats').html("Red: " + percentOffRed + "% off" + "<br>" +
                        "Green: " + percentOffGreen + "% off" + "<br>"  +
                        "Blue: " + percentOffBlue + "% off");
       alert("try again!");
@@ -143,6 +145,4 @@ function percentOff(guess, correct) {
 
     return false;
   });
-
-
 });
