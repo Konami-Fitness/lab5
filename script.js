ef423bd679a724d0ece8a0dfd3fa8a3488e90565
mkokkatt <47791277+mkokkatt@users.noreply.github.com>
2020-10-27 21:10:59 -0400

    timer,percentoff added

--- script.js
+++ script.js
@@ -1,169 +1,142 @@
 var jq = jQuery.noConflict();
-	jq(document).ready(function(){
+jq(document).ready(function(){
+    
 
-  var color = {
-  	red: 0,
-  	green: 0,
-  	blue: 0
+  var colorObj = {
+    red: 0,
+    green: 0,
+    blue: 0
   };
 
-  function setColor() {
-    color.red = Math.floor(Math.random() * 256); // range is 0-255, random doesnt include 1
-		color.green = Math.floor(Math.random() * 256);
-		color.blue = Math.floor(Math.random() * 256);
-		var thergb = "rgb(" + color.red + "," + color.green + "," + color.blue + ")";
-		jq(".colorswatch").css("background-color",thergb);
-		jq("#r1").html(thergb);
-	}
-  setColor();
+  var game = {
 
-  jq("#tred, #tgreen, #tblue").slider({
+//include timer and round tracker
+
+  }
+        
+    function get_elapsed_time_string(total_seconds) {
+  function pretty_time_string(num) {
+    return ( num < 10 ? "0" : "" ) + num;
+  }
+
+  var hours = Math.floor(total_seconds / 3600);
+  total_seconds = total_seconds % 3600;
+
+  var minutes = Math.floor(total_seconds / 60);
+  total_seconds = total_seconds % 60;
+
+  var seconds = Math.floor(total_seconds);
+
+  // Pad the minutes and seconds with leading zeros, if required
+  hours = pretty_time_string(hours);
+  minutes = pretty_time_string(minutes);
+  seconds = pretty_time_string(seconds);
+
+  // Compose the string for display
+  var currentTimeString = hours + ":" + minutes + ":" + seconds;
+
+  return currentTimeString;
+}
+
+var elapsed_seconds = 0;
+setInterval(function() {
+  elapsed_seconds = elapsed_seconds + 1;
+  $('#box_header').text(get_elapsed_time_string(elapsed_seconds));
+}, 1000);
+
+  function setColor() {
+    var x = Math.floor(Math.random() * 256); // range is 0-255
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var thergb = "rgb(" + x + "," + y + "," + z + ")";
+
+    var colors = new Array(x, y, z);
+    jq(".colorswatch").css("background-color",thergb);
+    colorObj.red = colors[0];
+    colorObj.green = colors[1];
+    colorObj.blue = colors[2];
+    //this line below will reveal value of randomly generated color
+   // jq("#r1").html(thergb);
+  }
+
+  setColor();
+  jq("#red-slide, #green-slide, #blue-slide").slider({
     orientation: "horizontal",
     range: "min",
     min: 0,
     max: 255,
-    value: 127,
-    slide: function( event, ui ) {
-      $( "#tred" ).val( ui.value );
-    }
+    value: 0
   });
-  jq("#tred").slider("value", color.red);
-  jq("#tgreen").slider("value", color.green);
-  jq("#tblue").slider("value", color.blue);
-/*
 
-	 function printColor() {
-	 	var arr = setColor();
+  
+  jq('#red').on('input', function() {
+    let input=jq(this);
+    jq('#red-slide').slider('value', input.val())
+  });
+  jq('#green').on('input', function() {
+    let input=jq(this);
+    jq('#green-slide').slider('value', input.val());
+  });
+  jq('#blue').on('input', function() {
+    let input=jq(this);
+    jq('#blue-slide').slider('value', input.val());
+  });
+  jq('#red-slide').slider({
+    change: function(event, ui) { 
+       jq("#red").val(ui.value)
+    } 
+  })
+  jq('#green-slide').slider({
+    change: function(event, ui) { 
+       jq("#green").val(ui.value)
+    } 
+  })
+  jq('#blue-slide').slider({
+    change: function(event, ui) { 
+       jq("#blue").val(ui.value)
+    } 
+  })
+    
 
-	   jq(".colorswatch").after("red: " + arr[0] + " green: " + arr[1] + " blue: " + arr[2]);
+function percentOff(guess, correct) {
 
-
-	 }
-
-			setColor();
-			printColor();*/
-/*
-
-
-jq('#red').on('input', function() {
-	var input=jq(this);
-	var color=input.val();
-	colorObj.red = color;
-
-});
-jq('#green').on('input', function() {
-	var input=jq(this);
-	var color=input.val();
-	colorObj.green = color;
-
-});
-jq('#blue').on('input', function() {
-	var input=jq(this);
-	var color=input.val();
-	colorObj.blue = color;
-
-});
-
+  return Math.round((((Math.abs(correct - guess) / 255) * 100) + Number.EPSILON) * 100) / 100;
+}
 
 
 
-
-
-
-
-var rgb = "rgb(" + values[0] + "," + values[1] + "," + values[2] + ")";
-
-jq(".colorswatch").css("background-color",rgb);
-
-*/
-
-  document.getElementById("colors").addEventListener("submit", function(e) {
+  document.getElementById('colors').addEventListener('submit', function(e) {
     e.preventDefault();
 
     // your code here
-    var rguess = document.getElementById('red').value;
-    var gguess = document.getElementById('green').value;
-    var bguess = document.getElementById('blue').value;
+    var x = document.getElementById('red').value;
+    var y = document.getElementById('green').value;
+    var z = document.getElementById('blue').value;
 
     // Calculation
-    var rgb = "rgb(" + rguess + "," + gguess + "," + bguess + ")";
+    var rgb = "rgb(" + x + "," + y + "," + z + ")";
     // Show Result
-    if(rguess == color.red && gguess == color.green && bguess == color.blue) {
-    	jq('html').css('background-color','black');
-    	jq('p#congrats').html("Congrats!").css({"color":"gold", "font-size":"100px", "text-align":"center"});
-    	jq('.colorswatch').hide();
-    	jq('#colors').hide();
-    	jq('h1').hide();
-    	alert("you won!");
+    if(x == colorObj.red && y == colorObj.green && z == colorObj.blue) {
+      jq('html').css('background-color','black');
+      jq('p#congrats').html("Congrats!").css({"color":"gold", "font-size":"100px", "text-align":"center"});
+      jq('.colorswatch').hide();
+      jq('#colors').hide();
+      jq('h1').hide();
+      alert("you won!");
     } else {
-    	alert("try again!");
+      var percentOffRed = percentOff(x, colorObj.red);
+
+      var percentOffGreen = percentOff(y, colorObj.green);
+      var percentOffBlue = percentOff(z, colorObj.blue);
+      jq('.stats').html("Red: " + percentOffRed + "% off" + "<br>" + 
+                       "Green: " + percentOffGreen + "% off" + "<br>"  +
+                       "Blue: " + percentOffBlue + "% off");
+      alert("try again!");
     }
     //jq(".colorswatch").css("background-color",rgb);
 
     return false;
   });
 
- /*
-    function hexFromRGB(r, g, b) {
-      var hex = [
-        r.toString( 16 ),
-        g.toString( 16 ),
-        b.toString( 16 )
-      ];
-      $.each( hex, function( nr, val ) {
-        if ( val.length === 1 ) {
-          hex[ nr ] = "0" + val;
-        }
-      });
-      return hex.join( "" ).toUpperCase();
-    }*/
 
-    /*
-function pickColor() {
-
-
-           var x = Math.floor(Math.random() * 256); // range is 0-255
-    		var y = Math.floor(Math.random() * 256);
-    		var z = Math.floor(Math.random() * 256);
-    		var colors = new Array(x, y, z);
-    		return colors;
-}*/
-/*
-function refreshSwatch() {
-      var red = $( "#red" ).slider( "value" ),
-        green = $( "#green" ).slider( "value" ),
-        blue = $( "#blue" ).slider( "value" ),
-        hex = hexFromRGB( red, green, blue );
-      $( "#colorswatch" ).css( "background-color", "#" + hex );
-    }
-
-$( "#red, #green, #blue" ).slider({
-      orientation: "horizontal",
-      range: "min",
-      min: 0,
-      max: 255,
-      value: 127,
-     slide: refreshSwatch(),
-      change: refreshSwatch()
-    });
-    var x = pickColor();
-    $( "#red" ).slider( "value", x[0] );
-    $( "#green" ).slider( "value",x[1] );
-    $( "#blue" ).slider( "value", x[2]);
-*/
-
-/*
-$( function() {
-    $( "#red" ).slider({
-      orientation: "vertical",
-      range: "min",
-      min: 0,
-      max: 255,
-      value: 60,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value );
-      }
-    });
-    $( "#amount" ).val( $( "#red" ).slider( "value" ) );
-  } );*/
 });
